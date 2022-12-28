be237db8c02db292256aff9c68ec9055edf30a51
zakariae belkhnati <58368229+zakichan719@users.noreply.github.com>
2022-12-28 19:55:47 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("#btnclose").click(function(){
+       $(".prop-form").hide(1000);
+      });
+      
+      
+      
+      $("#btnpro").click(function(){
+        $(".prop-form").show(1000);
+      });
+ 
+ 
+ });
+
+
+ 
